9690bced8f14163ad2cf8aa8af9f6cc3e8222cc5
lanpokn <2606629917@qq.com>
2024-01-21 14:54:35 +0800

    first version depth

--- gaussian_renderer/__init__.py
+++ gaussian_renderer/__init__.py
@@ -100,37 +100,126 @@ def render(viewpoint_camera, pc : GaussianModel, pipe, bg_color : torch.Tensor, 
             "visibility_filter" : radii > 0,
             "radii": radii}
 
-#a temp of using radii and visibility_filter to get a depth map
-#need to be change a lot , but at least we can using similar way to get the information
-#you may need to use a simple
+def project_points(points_3d, projection_matrix):
+    # 将3D点云投影到2D平面
+    points_3d_homogeneous = torch.cat((points_3d, torch.ones(points_3d.shape[0], 1)), dim=1)
+    points_2d_homogeneous = torch.matmul(points_3d_homogeneous, projection_matrix.t())
+    points_2d = points_2d_homogeneous[:, :2] / points_2d_homogeneous[:, 2].unsqueeze(1)
+    
+    return points_2d
 
-# def generate_depth_map(viewspace_points, radii):
-#     # 获取屏幕尺寸，假设为width x height
-#     width, height = get_screen_size()
+def generate_depth_map(points_3d, camera_center, projection_matrix, image_size):
+    # 计算相机到点云的向量
+    camera_to_points = points_3d - camera_center
 
-#     # 初始化深度图
-#     depth_map = np.zeros((height, width), dtype=float)
+    # 投影到图像平面
+    points_2d = project_points(points_3d, projection_matrix)
 
-#     for i in range(len(viewspace_points)):
-#         x, y = viewspace_points[i][:2]  # 获取屏幕坐标
-#         radius = radii[i]
+    # 获取图像尺寸
+    height, width = image_size
 
-#         # 确保点在屏幕范围内
-#         # x,y可以根据半径扩充，透明度也要根据高斯函数衰减
-#         if 0 <= x < width and 0 <= y < height:
-#             # 计算深度值，可以根据需要进行调整
-#             depth_value = calculate_depth_value(viewspace_points[i])
+    # 初始化深度图
+    depth_map = torch.zeros((height, width), dtype=torch.float32)
 
-#             # 将深度值存储到深度图中
-              #if depth_value<depth_map[int(y), int(x)]
-#             depth_map[int(y), int(x)] = depth_value
+    # 遍历投影后的点，计算深度图
+    for i in range(points_2d.shape[0]):
+        x, y = points_2d[i]
+        depth_value = torch.norm(camera_to_points[i])
 
-#     return depth_map
+        # 将深度值存储到深度图中
+        if 0 <= x < width and 0 <= y < height:
+            depth_map[int(y), int(x)] = depth_value
 
-# def calculate_depth_value(point):
-#     # 根据需要进行深度值计算，可能涉及到视锥体等
-#     # 这里简单地使用z坐标作为深度值
-      # need more complicated way to deal with it
-      # may suffer from noise
-      # if opacity is too small use it, do not locate it
-#     return point[2]
+    return depth_map
+
+def render_depth(viewpoint_camera, pc : GaussianModel, pipe, bg_color : torch.Tensor, scaling_modifier = 1.0, override_color = None):
+    """
+    Render the scene. 
+    
+    Background tensor (bg_color) must be on GPU!
+    """
+ 
+    # Create zero tensor. We will use it to make pytorch return gradients of the 2D (screen-space) means
+    screenspace_points = torch.zeros_like(pc.get_xyz, dtype=pc.get_xyz.dtype, requires_grad=True, device="cuda") + 0
+    try:
+        screenspace_points.retain_grad()
+    except:
+        pass
+
+    # Set up rasterization configuration
+    tanfovx = math.tan(viewpoint_camera.FoVx * 0.5)
+    tanfovy = math.tan(viewpoint_camera.FoVy * 0.5)
+
+    raster_settings = GaussianRasterizationSettings(
+        image_height=int(viewpoint_camera.image_height),
+        image_width=int(viewpoint_camera.image_width),
+        tanfovx=tanfovx,
+        tanfovy=tanfovy,
+        bg=bg_color,
+        scale_modifier=scaling_modifier,
+        viewmatrix=viewpoint_camera.world_view_transform,
+        projmatrix=viewpoint_camera.full_proj_transform,
+        sh_degree=pc.active_sh_degree,
+        campos=viewpoint_camera.camera_center,
+        prefiltered=False,
+        debug=pipe.debug
+    )
+
+    rasterizer = GaussianRasterizer(raster_settings=raster_settings)
+
+    means3D = pc.get_xyz
+    means2D = screenspace_points
+    opacity = pc.get_opacity
+
+    # If precomputed 3d covariance is provided, use it. If not, then it will be computed from
+    # scaling / rotation by the rasterizer.
+    scales = None
+    rotations = None
+    cov3D_precomp = None
+    if pipe.compute_cov3D_python:
+        cov3D_precomp = pc.get_covariance(scaling_modifier)
+    else:
+        scales = pc.get_scaling
+        rotations = pc.get_rotation
+
+    # If precomputed colors are provided, use them. Otherwise, if it is desired to precompute colors
+    # from SHs in Python, do it. If not, then SH -> RGB conversion will be done by rasterizer.
+    shs = None
+    colors_precomp = None
+    if override_color is None:
+        if pipe.convert_SHs_python:
+            shs_view = pc.get_features.transpose(1, 2).view(-1, 3, (pc.max_sh_degree+1)**2)
+            dir_pp = (pc.get_xyz - viewpoint_camera.camera_center.repeat(pc.get_features.shape[0], 1))
+            dir_pp_normalized = dir_pp/dir_pp.norm(dim=1, keepdim=True)
+            sh2rgb = eval_sh(pc.active_sh_degree, shs_view, dir_pp_normalized)
+            colors_precomp = torch.clamp_min(sh2rgb + 0.5, 0.0)
+        else:
+            shs = pc.get_features
+    else:
+        colors_precomp = override_color
+
+    # Rasterize visible Gaussians to image, obtain their radii (on screen). 
+    #radii:radio of gaussian in image
+    rendered_image, radii = rasterizer(
+        means3D = means3D,
+        means2D = means2D,
+        shs = shs,
+        colors_precomp = colors_precomp,
+        opacities = opacity,
+        scales = scales,
+        rotations = rotations,
+        cov3D_precomp = cov3D_precomp)
+    visibility_filter = radii > 0
+    height=int(viewpoint_camera.image_height),
+    width=int(viewpoint_camera.image_width),
+    depth_map = torch.zeros((height, width), dtype=torch.float32)
+
+    # 示例使用
+    # 假设 camera_center, projection_matrix, points_3d 是已知的
+    # image_size 是图像的尺寸，比如 (640, 480)
+    points_3d = means3D
+    camera_center = viewpoint_camera.camera_center
+    projection_matrix = viewpoint_camera.full_proj_transform
+    image_size = [int(viewpoint_camera.image_height),int(viewpoint_camera.image_width)]
+    depth_map = generate_depth_map(points_3d, camera_center, projection_matrix, image_size)
+    return depth_map
