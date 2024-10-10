82eed825ced2587c213c60eaf721a02b2012a745
lanpokn <2606629917@qq.com>
2024-10-10 16:47:36 +0800

    delete useless file

--- train.py
+++ train.py
@@ -22,10 +22,10 @@ from tqdm import tqdm
 from utils.image_utils import psnr,LPIPS
 from argparse import ArgumentParser, Namespace
 from arguments import ModelParams, PipelineParams, OptimizationParams
-from Event_sensor.event_tools import *
-import copy
-from Event_sensor.src.event_buffer import EventBuffer
-import torchvision
+# from Event_sensor.event_tools import *
+# import copy
+# from Event_sensor.src.event_buffer import EventBuffer
+# import torchvision
 from render import Generate_new_view
 try:
     from torch.utils.tensorboard import SummaryWriter
@@ -41,42 +41,42 @@ def generate_random_integer_nearby(target_integer, range_half_width):
     lower_bound = target_integer - range_half_width
     upper_bound = target_integer + range_half_width
     return random.randint(lower_bound, upper_bound)
-class CameraPoseInterpolator:
-    def __init__(self, camera_poses,dt):
-        self.camera_poses = camera_poses
-        self.dt = dt
+# class CameraPoseInterpolator:
+#     def __init__(self, camera_poses,dt):
+#         self.camera_poses = camera_poses
+#         self.dt = dt
 
-    def interpolate_pose_at_time(self, t):
-        """
-        Interpolates camera pose at a given time t.
-        more range to avoid out of range
-        """
-        for idx in range(-10,len(self.camera_poses) + 10):
-            if idx <0:
-                view = self.camera_poses[0]
-                view_next = self.camera_poses[1]
-            else:
-                view = self.camera_poses[len(self.camera_poses)-1]
-                view_next = self.camera_poses[len(self.camera_poses)-2]
+#     def interpolate_pose_at_time(self, t):
+#         """
+#         Interpolates camera pose at a given time t.
+#         more range to avoid out of range
+#         """
+#         for idx in range(-10,len(self.camera_poses) + 10):
+#             if idx <0:
+#                 view = self.camera_poses[0]
+#                 view_next = self.camera_poses[1]
+#             else:
+#                 view = self.camera_poses[len(self.camera_poses)-1]
+#                 view_next = self.camera_poses[len(self.camera_poses)-2]
 
-            view = self.camera_poses[idx]
-            view_next = self.camera_poses[idx + 1]
+#             view = self.camera_poses[idx]
+#             view_next = self.camera_poses[idx + 1]
 
-            if t >= self.dt*idx and t <= self.dt*(idx+1):
-                alpha = (t - self.dt*idx) / (self.dt)
+#             if t >= self.dt*idx and t <= self.dt*(idx+1):
+#                 alpha = (t - self.dt*idx) / (self.dt)
 
-                q_start = rotation_matrix_to_quaternion(view.R)
-                q_end = rotation_matrix_to_quaternion(view_next.R)
-                T_start = view.T
-                T_end = view_next.T
+#                 q_start = rotation_matrix_to_quaternion(view.R)
+#                 q_end = rotation_matrix_to_quaternion(view_next.R)
+#                 T_start = view.T
+#                 T_end = view_next.T
 
-                q_temp = Nlerp(q_end, q_start, alpha)
-                q_temp /= np.linalg.norm(q_temp)
-                R_temp = quaternion_to_rotation_matrix(q_temp)
+#                 q_temp = Nlerp(q_end, q_start, alpha)
+#                 q_temp /= np.linalg.norm(q_temp)
+#                 R_temp = quaternion_to_rotation_matrix(q_temp)
 
-                T_temp = Nlerp(T_end, T_start, alpha)
-                view_temp = Generate_new_view(view,R_temp,T_temp)
-                return view_temp
+#                 T_temp = Nlerp(T_end, T_start, alpha)
+#                 view_temp = Generate_new_view(view,R_temp,T_temp)
+#                 return view_temp
 def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoint_iterations, checkpoint, debug_from,event_path):
     first_iter = 0
     tb_writer = prepare_output_and_logger(dataset)
@@ -105,7 +105,7 @@ def training(dataset, opt, pipe, testing_iterations, saving_iterations, checkpoi
     first_iter += 1
     #TODO first generate a camera position according to scene.getTrainCameras().copy()
     #use t as indicator
-    Interpolator = CameraPoseInterpolator(scene.getTrainCameras(),13513*10)
+    # Interpolator = CameraPoseInterpolator(scene.getTrainCameras(),13513*10)
     # 定义一个新的优化器，仅用于优化参数 c
     c = torch.nn.Parameter(torch.tensor(0.17))
     optimizer_c = optim.Adam([c], lr=0.1)  # 设置 c 的学习率为 0.001
