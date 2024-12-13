53d663f8aa13d6f3c0733d676aed6b2f6cc5d14d
lanpokn <62628201+lanpokn@users.noreply.github.com>
2024-12-13 17:08:34 +0800

    Update generate_dataset.py

--- generate_dataset.py
+++ generate_dataset.py
@@ -1,4 +1,5 @@
-from Event_sensor.event_tools import *
+#It has been deprecated due to the poor stability of the pure integration method.
+#from Event_sensor.event_tools import *
 
 # generate_images_accumu_eds("D:/2024/3DGS/Nerf_Event/data/eds/09_ziggy_flying_pieces/",13513*10,140,1)
 # generate_images_accumu_edslike("D:/2024/3DGS/Nerf_Event/data/eds/09_ziggy_flying_pieces/",13513*10,140,1)
@@ -6,5 +7,5 @@ from Event_sensor.event_tools import *
 # generate_images_accumu_T("D:/2024/3DGS/e2calib/trans_1mps_H_R2L",21153,51,1)
 # generate_images_accumu_Tumvie("D:/2024/3DGS/Nerf_Event/data/tumvie/mocap-desk2-events_left",52631,424,1)
 # generate_images("D:/2024/3DGS/dataset/nerf_synthetic/lego/raw2",200,67,800,800)
-generate_images_accumu("D:/2024/3DGS/dataset/nerf_synthetic/mic_colmap_easy/Esim",1000,104)
-generate_images_accumu_volt("D:/2024/3DGS/dataset/nerf_synthetic/mic_colmap_easy/",1000,104)
+# generate_images_accumu("D:/2024/3DGS/dataset/nerf_synthetic/mic_colmap_easy/Esim",1000,104)
+# generate_images_accumu_volt("D:/2024/3DGS/dataset/nerf_synthetic/mic_colmap_easy/",1000,104)
